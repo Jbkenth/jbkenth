56493ed9f69f3951345614ff1e176c6da1eb1eed
Jbkenth <148613718+Jbkenth@users.noreply.github.com>
2024-10-27 13:39:50 +0800

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,1 +1,13 @@
-importScripts("https://js.pusher.com/beams/service-worker.js");
+// service-worker.js
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon || '/default-icon.png',
+        badge: data.badge || '/badge-icon.png',
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
